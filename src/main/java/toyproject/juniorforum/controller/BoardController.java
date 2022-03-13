80fcf3761fab45e345b73f7735cc39e2b14bbdb7
code-tree <dltjdqls1200@naver.com>
2022-03-14 00:30:04 +0900

    수정 컨트롤러, 타임리프 완성
    예정)
    삭제
    검색, 댓글 기능, 파입 업로드, 로그인 처리 (이제 기능 확장 최소한)

--- src/main/java/toyproject/juniorforum/controller/BoardController.java
+++ src/main/java/toyproject/juniorforum/controller/BoardController.java
@@ -66,35 +66,34 @@ public class BoardController {
         redirectAttributes.addAttribute("boardId", boardDTO.getBoardId());
         redirectAttributes.addAttribute("status", true);
         log.info("create controller");
-        return "redirect:/board/list";
+        return "redirect:/board/read";
     }
 
     @GetMapping({"/read", "/update"})
     public String read(Criteria criteria, int boardId, Model model, HttpServletRequest request) {
         log.info("read controller");
-
-        model.addAttribute("board", boardService.read(boardId));
         model.addAttribute("criteria", criteria);
+        String requestMapping = request.getRequestURI().substring(7);
+        BoardVO boardVO = boardService.read(boardId);
+        if (requestMapping.equals("update")) {
+            BoardUpdateForm boardUpdateForm = boardVO.convertToUpdateDTO();
+            log.info("boardId = {}", boardUpdateForm.getBoardId());
+            model.addAttribute("board", boardUpdateForm);
+            return "board/update";
+        }
+        model.addAttribute("board", boardVO);
         return "board/read";
     }
 
-    @GetMapping("/{boardId}/update")
-    public String updateForm(@PathVariable int boardId, Model model) {
-        model.addAttribute("board", boardService.read(boardId).convertToUpdateDTO());
-        log.info("updateForm controller");
-        return "board/update";
-    }
-
-    @PostMapping("/{boardId}/update")
-    public String update(@Validated @ModelAttribute("board") BoardUpdateForm board, BindingResult bindingResult, Model model) {
+    @PostMapping("/update")
+    public String update(@Validated @ModelAttribute("board") BoardUpdateForm board, BindingResult bindingResult,
+                         Criteria criteria) {
         if (bindingResult.hasErrors()) {
             log.info("errors = {}", bindingResult);
             return "board/update";
         }
         boardService.update(board);
-        model.addAttribute("boardList", boardService.getList(new Criteria()));
-        log.info("update controller");
-        return "board/list";
+        return "redirect:/board/list" + criteria.getListLink();
     }
 
 }
